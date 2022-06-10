55f5de5596e7cc3f17cebce215a18e48723efbcf
Singhik <89758324+singhic@users.noreply.github.com>
2022-06-10 10:10:36 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string input;
+
+	while (1)
+	{
+		getline(cin, input);
+		if (input == "")
+		{
+			break;
+		}
+		cout << input << endl;
+	}
+	return 0;
+}
